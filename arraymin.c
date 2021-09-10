bc6f7ecf981baa767f122216bd2f476101dbfeca
pranavjoshi8240 <90453829+pranavjoshi8240@users.noreply.github.com>
2021-09-10 16:43:24 +0530

    array find min max value

---
+++ arraymin.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    //array 
+    //datatype variable-name = {value}
+    long id[5] = {100,25,8,-10,66};
+    long min=id[0];
+    int position=1;
+    
+    printf("Hello world...");
+}
